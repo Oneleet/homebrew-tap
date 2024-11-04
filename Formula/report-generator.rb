9662a2f622191e75057b621570ee8d2d07c09192
Mohammed Nafees <hello@mnafees.me>
2024-11-04 18:33:34 +0530

    fix version without prefix v

--- Formula/report-generator.rb
+++ Formula/report-generator.rb
@@ -4,7 +4,7 @@ require_relative "lib/private_strategy"
 class ReportGenerator < Formula
   desc "Oneleet Report Generator"
   homepage "https://github.com/oneleet/report-generator"
-  version "v1.1.0"
+  version "1.1.0"
   
   if OS.mac?
     url "https://github.com/Oneleet/report-generator/releases/download/v1.1.0/report-generator", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
@@ -13,5 +13,9 @@ class ReportGenerator < Formula
   
   def install
     bin.install "report-generator"
+  end
+  
+  test do
+    system "#{bin}/report-generator", "--version"
   end
 end
