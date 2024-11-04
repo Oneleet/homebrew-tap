e2a9fbc8f31e3e504dd1883fb952256b2fa1be15
Mohammed Nafees <hello@mnafees.me>
2024-11-04 13:39:22 +0530

    initial commit

---
+++ Formula/report-generator.rb
@@ -0,0 +1,18 @@
+class ReportGenerator < Formula
+  desc "Oneleet Report Generator"
+  homepage "https://github.com/oneleet/report-generator"
+  version "v1.1.0"
+  
+  if OS.mac?
+    url "https://github.com/oneleet/report-generator/releases/download/refs/tags/v1.1.0/report-generator", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "${BINARY_SHA256}"
+  end
+  
+  def install
+    bin.install "report-generator"
+  end
+  
+  test do
+    system "#{bin}/report-generator", "--version"
+  end
+end
